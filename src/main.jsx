e5f8c61b4af65d5616afc1d06b9d47f07a0c7354
deepika ravikumar <deepikaravikumar26@gmail.com>
2024-07-19 14:38:58 +0530

    New

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/HelloWorldComponent/HelloWorldComponent'
+import App from './components/HelloWorldComponent/HelloWorldComponent.jsx'
 import './components/HelloWorldComponent/HelloWorldComponent.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
